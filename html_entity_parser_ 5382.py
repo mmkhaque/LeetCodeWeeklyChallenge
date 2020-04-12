b155389a90c1bf460d40828369ffa81878cfd157
codeingreen <codeingreen@outlook.com>
2020-04-11 23:57:12 -0400

    Create html_entity_parser_ 5382.py

---
+++ html_entity_parser_ 5382.py
@@ -0,0 +1,34 @@
+import re
+
+class Solution:
+    def entityParser(self, text: str) -> str:
+        
+        if text is None:
+            return ''
+        
+        if len(text) == 1:
+            return text
+        
+        '''
+        Quotation Mark: the entity is &quot; and symbol character is ".
+        Single Quote Mark: the entity is &apos; and symbol character is '.
+        Ampersand: the entity is &amp; and symbol character is &.
+        Greater Than Sign: the entity is &gt; and symbol character is >.
+        Less Than Sign: the entity is &lt; and symbol character is <.
+        Slash: the entity is &frasl; and symbol character is /.
+        '''
+        
+        text = re.sub(r'&quot;', '\"', text)
+        text = re.sub(r'&apos;', '\'', text)
+        text = re.sub(r'&amp;', '&', text)
+        text = re.sub(r'&gt;', '>', text)
+        text = re.sub(r'&lt;', '<', text)
+        text = re.sub(r'&frasl;', '/', text)
+        
+        print(text)
+        
+        return text
+                     
+        
+        
+        
