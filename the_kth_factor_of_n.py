233e0183a5d32b1567fd08d53d9cdf6eae3196ad
codeingreen <codeingreen@outlook.com>
2020-06-27 11:51:44 -0400

    Create the_kth_factor_of_n.py

---
+++ the_kth_factor_of_n.py
@@ -0,0 +1,19 @@
+class Solution:
+    def kthFactor(self, n: int, k: int) -> int:
+        
+        factors = []
+        
+        for i in range(1, n+1):
+            
+            if n % i == 0:
+                factors.append(i)
+        
+        if len(factors) < k:
+            return -1
+        
+        print(factors)
+        print(factors[k-1])
+        
+        return factors[k-1]
+            
+        
